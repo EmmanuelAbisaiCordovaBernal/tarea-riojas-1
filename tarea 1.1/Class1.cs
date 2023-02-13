0328d79412122c42940301c026734b08078be0c9
Emman <Emman@DESKTOP-H4H8K9D>
2023-02-12 18:12:38 -0600

    Agregar archivos de proyecto.

---
+++ tarea 1.1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  Class1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            
+            Console.WriteLine("Ingrese sus apellidos");
+            string apellidos = Console.ReadLine();
+
+            Console.WriteLine(" Ingrese su genero");
+            string genero = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Infrese su nacionalidad");
+            string nacionalidad = Console.ReadLine();
+
+            Console.WriteLine(" Bienvenido " + nombre + " " + apellidos + " su genero es " + genero + " su edad es de " + edad + " años " + " y su nacionalidas es " + nacionalidad);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tarea 1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea_1._1
+{
+    class Class1
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese sus apellidos");
+            string apellidos = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Bienvenido " + nombre + apellidos + " su edad es de " + edad + " años ");
+            Console.ReadKey();
+        }
+    }
+}
